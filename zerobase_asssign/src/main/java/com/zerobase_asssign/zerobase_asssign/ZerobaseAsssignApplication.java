d0d51e1d924928bc43a362838bfb0f172ab58b3a
sejin\admin <seijin0722@naver.com>
2023-01-25 03:30:06 +0900

    Initial commit

---
+++ zerobase_asssign/src/main/java/com/zerobase_asssign/zerobase_asssign/ZerobaseAsssignApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase_asssign.zerobase_asssign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerobaseAsssignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZerobaseAsssignApplication.class, args);
+	}
+
+}
---
+++ zerobase_asssign/src/test/java/com/zerobase_asssign/zerobase_asssign/ZerobaseAsssignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase_asssign.zerobase_asssign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerobaseAsssignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
